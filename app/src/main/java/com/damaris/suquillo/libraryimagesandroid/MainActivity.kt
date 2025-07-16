fb79c88723a4a03808b65f0fdab69b135c92ff4f
dakookie <damaris.suquillo@epn.edu.ec>
2025-07-16 13:52:33 -0500

    Add Activities, implements necesary dependences and basic design of MainActivity

--- app/src/main/java/com/damaris/suquillo/libraryimagesandroid/MainActivity.kt
+++ app/src/main/java/com/damaris/suquillo/libraryimagesandroid/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.damaris.suquillo.libraryimagesandroid
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonGlide: Button
+    lateinit var buttonPicasso: Button
+    lateinit var buttonCoil: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //Redirección del botón Glide
+        buttonGlide = findViewById(R.id.buttonGlide)
+        buttonGlide.setOnClickListener {
+            val intent = Intent(this, GlideActivity::class.java)
+            startActivity(intent)
+        }
+
+        //Redirección del botón Picasso
+        buttonPicasso = findViewById(R.id.buttonPicasso)
+        buttonPicasso.setOnClickListener {
+            val intent = Intent(this, PicassoActivity::class.java)
+            startActivity(intent)
+        }
+
+        //Redirección del botón Coil
+        buttonCoil = findViewById(R.id.buttonCoil)
+        buttonCoil.setOnClickListener {
+            val intent = Intent(this, CoilActivity::class.java)
+            startActivity(intent)
         }
     }
 }
